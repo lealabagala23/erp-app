96bfe2b9b310343062211ca8c10ddfdddbff5e7f
Lea Labagala <llabagala@zetaglobal.com>
2024-10-30 22:05:19 +0800

    Add sign in tenplate

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './SignIn';
 import reportWebVitals from './reportWebVitals';
 import { StyledEngineProvider } from '@mui/material';
 
