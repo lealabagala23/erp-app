97b7d105d1471c862fa21b6735bd221d02d786a3
Lea Labagala <llabagala@zetaglobal.com>
2024-11-09 13:15:25 +0800

    Fix cors issue

--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
+app.options("*", cors());
+
 app.use(express.json());
 app.use(bodyParser.json());
 
