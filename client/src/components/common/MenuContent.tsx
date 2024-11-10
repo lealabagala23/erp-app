d1726d4a4297a5791f6bf73db70e9b5713e7482b
Lea Labagala <llabagala@zetaglobal.com>
2024-11-10 20:27:09 +0800

    Add collapsible side nav item

--- client/src/components/common/MenuContent.tsx
+++ client/src/components/common/MenuContent.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -12,6 +12,8 @@ import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import { MoneyRounded } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
+// import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
+// import { GridExpandMoreIcon } from '@mui/x-data-grid';
 
 const mainListItems = [
   { text: 'Home', icon: <HomeRoundedIcon />, route: '/home' },
@@ -31,6 +33,13 @@ const secondaryListItems = [
 
 export default function MenuContent() {
   const navigate = useNavigate();
+  const [expanded, setExpanded] = useState<string | boolean>(false);
+
+  const handleAccordionChange =
+    (panel: string) => (e: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+
   return (
     <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
       <List dense>
@@ -45,6 +54,65 @@ export default function MenuContent() {
             </ListItemButton>
           </ListItem>
         ))}
+        {/* <ListItem
+          key={mainListItems.length}
+          disablePadding
+          sx={{ display: 'block' }}
+        >
+          <Accordion
+            expanded={expanded === 'panel1'}
+            onChange={handleAccordionChange('panel1')}
+            disableGutters
+            elevation={0}
+            sx={{
+              boxShadow: 'none', // Remove shadow
+              backgroundColor: 'transparent', // Remove background color
+              border: 'none', // Remove border
+              padding: 0,
+              '& .MuiAccordionSummary-root': {
+                minHeight: 'unset',
+                padding: 0,
+              },
+            }}
+          >
+            <AccordionSummary
+              aria-controls="panel1-content"
+              id="panel1-header"
+              sx={{
+                margin: 0,
+                '> .MuiAccordionSummary-content': { margin: 0 },
+                '.MuiListItemButton-root': { opacity: 1 },
+              }}
+            >
+              <ListItemButton
+                selected={mainListItems.length === 0}
+                onClick={() => navigate('/inventory')}
+                sx={{ padding: 0 }}
+              >
+                <ListItemIcon>
+                  <AssignmentRoundedIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Inventory'} />
+              </ListItemButton>
+            </AccordionSummary>
+            <AccordionDetails sx={{ paddingTop: 0, paddingBottom: 0 }}>
+              <List sx={{ paddingTop: 0, paddingBottom: 0 }}>
+                <ListItem
+                  key={mainListItems.length + 1}
+                  disablePadding
+                  sx={{ display: 'block' }}
+                >
+                  <ListItemButton
+                    selected={mainListItems.length + 1 === 0}
+                    onClick={() => navigate('/orders')}
+                  >
+                    <ListItemText primary={'Orders'} />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </AccordionDetails>
+          </Accordion>
+        </ListItem> */}
       </List>
 
       <List dense>
