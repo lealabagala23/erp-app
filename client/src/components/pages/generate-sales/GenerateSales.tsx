b88ff6e28a2486eb369fcee9c28bba3d910d4014
Lea Labagala <llabagala@zetaglobal.com>
2025-04-03 19:31:55 +0800

    Use blank pdf for invoice

--- client/src/components/pages/generate-sales/GenerateSales.tsx
+++ client/src/components/pages/generate-sales/GenerateSales.tsx
@@ -27,9 +27,9 @@ import FormAutocomplete from '../../common/FormAutocomplete';
 import ItemTable from './ItemTable';
 import { CancelItem, Order, OrderItem, Payment, TableItem } from './types';
 import { fetchProducts } from '../inventory/apis';
-import lhctPDF from '../../../assets/lhct_invoice.pdf';
-import lmtPDF from '../../../assets/lmt_invoice.pdf';
-// import blankPDF from '../../../assets/blank.pdf';
+// import lhctPDF from '../../../assets/lhct_invoice.pdf';
+// import lmtPDF from '../../../assets/lmt_invoice.pdf';
+import blankPDF from '../../../assets/blank.pdf';
 import { formatCurrency } from '../../../utils/auth';
 import {
   Approval,
@@ -392,7 +392,7 @@ export default function GenerateSales() {
                       fontWeight="bold"
                       marginRight="4px"
                     >
-                      Transaction Date:
+                      Date:
                     </Typography>
                     <Controller
                       name="transaction_date"
@@ -972,9 +972,10 @@ export default function GenerateSales() {
                 }}
                 onClick={() => {
                   modifyPdf(
-                    activeCompany?.company_name?.includes('LHCT')
-                      ? lhctPDF
-                      : lmtPDF,
+                    // activeCompany?.company_name?.includes('LHCT')
+                    //   ? lhctPDF
+                    //   : lmtPDF,
+                    blankPDF,
                     order,
                   );
                   if (order.status === OrderStatus.APPROVED) {
